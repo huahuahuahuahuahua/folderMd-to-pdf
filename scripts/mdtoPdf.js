f6a24073f677de9aa4be3325a67182e8b0f74553
huawink <1017739427@qq.com>
2022-07-01 23:25:43 +0800

    feat:init

--- scripts/mdtoPdf.js
+++ scripts/mdtoPdf.js
@@ -1,28 +1,36 @@
-const fs = require('fs');
-const { mdToPdf } = require('md-to-pdf');
-const paths = require('./paths');
-
-async function mdtoPdf(inputMdPath,outputPdfPath) {
-    const pdf = await mdToPdf({ path: inputMdPath }, {
-        highlight_style: "monokai",//color-brewer
-        body_class: ["dark", "content"],
-        // css: `body { color: tomato; }`,
-        // css: `\`${require("../config/mdConfig.css")}\``,
-        pdf_options: {
-            format: "A4",
-            margin: "10mm",
-            printBackground: true
-        },
-        stylesheet_encoding: 'utf-8',
-        stylesheet: `${paths.config}\\vue.css`,
-    }).catch(console.error);
-    if (pdf) {
-		fs.writeFileSync(outputPdfPath, pdf.content);
+const fs = require("fs");
+const { mdToPdf } = require("md-to-pdf");
+const paths = require("./paths");
+/**
+ * @msg:
+ * @param {*} inputMdPath 输入的md文件绝对路径
+ * @param {*} outputPdfPath 输出的pdf文件绝对路径
+ * @return {*}
+ * @Descripttion:md-to-pdf
+ */
+async function mdtoPdf(inputMdPath, outputPdfPath) {
+  const pdf = await mdToPdf(
+    { path: inputMdPath },
+    {
+      highlight_style: "monokai", //color-brewer
+      body_class: ["dark", "content"],
+      // css: `body { color: tomato; }`,
+      // css: `\`${require("../config/mdConfig.css")}\``,
+      pdf_options: {
+        format: "A4",
+        margin: "10mm",
+        printBackground: true,
+      },
+      stylesheet_encoding: "utf-8",
+      stylesheet: `${paths.config}\\vue.css`,
     }
+  ).catch(console.error);
+  if (pdf) {
+    fs.writeFileSync(outputPdfPath, pdf.content);
+  }
 }
 module.exports = {
-    mdtoPdf
-}
+  mdtoPdf,
+};
 
-
-console.log(`${paths.config}\\vue.css`)
+console.log(`${paths.config}\\vue.css`);
